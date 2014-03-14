e46a66d614c215b603d7f5197e6ab8bb13cb7062
José Mussa <josemussa@uvicate.com>
2014-03-13 21:40:58 -0700

    Added a watch
    just messing around, maybe could be cool to use your Petbble as a watch
    while it is sleeping

--- src/menu.c
+++ src/menu.c
@@ -45,6 +45,8 @@ static AppTimer *timer;
 
 static bool animationPetPaused = false;
 
+TextLayer *time_layer; // The clock
+
 static void handle_tick(struct tm *tick_time, TimeUnits units_changed);
 
 static void clear_buttons(){
@@ -136,8 +138,23 @@ static void lightsOff(){
     animationPetPaused = true;
         
     bitmap_layer_set_bitmap(pet_layer, lightoff);
+        
     //bitmap_layer_set_bitmap(pet_layer, pet_sprites[14]);
-    
+        
+        
+        time_layer = text_layer_create(GRect(29, 54, 144-40 /* width */, 168-54 /* height */));
+        text_layer_set_text_color(time_layer, GColorWhite);
+        text_layer_set_background_color(time_layer, GColorClear);
+        text_layer_set_font(time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+        
+        // Ensures time is displayed immediately (will break if NULL tick event accessed).
+        // (This is why it's a good idea to have a separate routine to do the update itself.)
+        time_t now = time(NULL);
+        struct tm *current_time = localtime(&now);
+        handle_tick(current_time, SECOND_UNIT);
+        tick_timer_service_subscribe(SECOND_UNIT, &handle_tick);
+        
+        layer_add_child(window_get_root_layer(window), text_layer_get_layer(time_layer));
         
     }else{
     
@@ -300,7 +317,16 @@ static void animationPet(){
 
 static void handle_tick(struct tm *tick_time, TimeUnits units_changed)
 {
+    if(animationPetPaused){
 
+    static char time_text[] = "00:00:00"; // Needs to be static because it's used by the system later.
+    
+    
+    strftime(time_text, sizeof(time_text), "%T", tick_time);
+    text_layer_set_text(time_layer, time_text);
+    }
+    
+    
    ++animationCounter;
     switch (pet_level) {
         case 0:
@@ -367,6 +393,8 @@ static void destroyIcons(){
 
     bitmap_layer_destroy(pet_layer);
     
+    text_layer_destroy(time_layer);
+    window_destroy(window);
     
     
 }
